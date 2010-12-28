e13d965deb3c14bf4a51ccd27ead4b7bf4ab6255
Tom Usher <tom@4dmm.net>
2010-12-28 19:49:48 +0000

    Changed templating engine to mustache, updated QA motes to support correct answers, implemented basic two-way communication, change Node.js redis client to node_redis

--- flux/static/res/base.js
+++ flux/static/res/base.js
@@ -1,30 +1,36 @@
-$(document).ready(function() {
-    function message(obj){
-        var el = document.createElement('p');
-        el.innerHTML = obj.message;
-        console.log(obj.message);
-        document.getElementById('messages').appendChild(el);
-    }
+var socket = new io.Socket(null, {port: 80});
 
-    var socket = new io.Socket(null, {port: 80});
+$(document).ready(function() {
     socket.connect();
     socket.on("message", function(obj){
         console.log("message");
         update(obj.message);
     });
-
-    function update(obj) {
-        script = $("<script>");
-        script.attr("src", "templates/"+obj.content_type+".js");
-        script.attr("type", "text/javascript");
-        $("head").append(script);
-        $.get("templates/"+obj.content_type+".html", function(template) {
-            output = $.tmpl(template, obj);
-            $("body").html(output);
-            fn = window[obj.content_type];
-            if(typeof fn === 'function') {
-                fn();
-            }
-        });
-    }
 });
+
+function message(obj){
+    var el = document.createElement('p');
+    el.innerHTML = obj.message;
+    console.log(obj.message);
+    document.getElementById('messages').appendChild(el);
+}
+
+function update(obj) {
+    script = $("<script>");
+    script.attr("src", "templates/"+obj.content_type+".js");
+    script.attr("type", "text/javascript");
+    $("head").append(script);
+    $.get("templates/"+obj.content_type+".html", function(template) {
+        output = $.mustache(template, obj);
+        $("body").html(output);
+        fn = window[obj.content_type];
+        if(typeof fn === 'function') {
+            fn();
+        }
+    });
+}
+
+function respond(obj) {
+    socket.send(JSON.stringify(obj));
+}
+
