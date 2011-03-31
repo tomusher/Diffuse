77ff6e1b58165a08f8c59ce7f5e591d1e8efa3e3
Tom Usher <tom@4dmm.net>
2011-03-31 15:23:20 +0100

    Started refactoring admin JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,5 @@
 var socket = new io.Socket("flux.tomusher.com", {port: 80});
-var active_mote;
 var active_plan="";
-var renderer=undefined;
 
 $(document).ready(function(){
     active_plan = $("#plans .active").attr("data-id");
@@ -17,36 +15,9 @@ $(document).ready(function(){
     });
     socket.connect();
 
-    $(document).bind('serverPushedResponse', function(event, data) {
-        if(active_mote.pk == data.mote_id) {
-            var responses = {};
-            responses[data.client] = data.message;
-            if(renderer) {
-                renderer.updateData(responses, true);
-            }
-        }
-    });
-
-    $(document).bind('clientDisconnected', function(event, data) {
-        if(renderer) {
-            renderer.clientDisconnected(data.client);
-        }
-    });
-
-    $(document).bind('serverSentResponses', function(event, data) {
-        var responses = {};
-        $.each(data.response, function(client, response) {
-            try {
-                var response = JSON.parse(response);
-                responses[client] = response;
-            } catch(e) {}
-        });
-        if(renderer) {
-            renderer.updateData(responses, false);
-        }
-    });
-
-    $(document).bind('serverSetPlan', function(event, data) {
+    render_manager = new RenderManager();
+ 
+     $(document).bind('serverSetPlan', function(event, data) {
         setPushedMote(data.latest_mote);
     });
 
@@ -86,31 +57,80 @@ $(document).ready(function(){
         return false;
     });
 
-    function setPushedMote(active_mote) {
-        display(active_mote);
+    function setPushedMote(mote) {
+        render_manager.setActive(mote);
         socket.send(
         {event: "adminRequestedResponses",
                 data: {
-                    mote_id: active_mote.pk,
+                    mote_id: mote.pk,
                     plan: "plan:"+active_plan 
                  }
             });
         $(".mote-list li").each(function() {
             $("a", this).removeClass('active');
         });
-        $(".mote-list li a[data-id="+active_mote.pk+"]").addClass('active');
+        $(".mote-list li a[data-id="+mote.pk+"]").addClass('active');
     }
 
     function clearResponses() {
-        socket.send({event: "adminClearedResponses", data: {plan: "plan:"+active_plan, mote_id: active_mote.pk}});
+        socket.send({event: "adminClearedResponses", data: {plan: "plan:"+active_plan, mote_id: render_manager.active_mote.pk}});
     };
 
-    function display(active_mote) {
-        if(window[active_mote.content_type]) {
-            renderer = new window[active_mote.content_type](active_mote);
-            renderer.render();
-        }
-    }
 });
 
+function RenderManager() {
+    this.active_mote;
+    this.renderer;
 
+    var self = this;
+    $(document).bind('serverPushedResponse', function(event, data) {
+        self.newResponse(data);
+    });
+    $(document).bind('serverSentResponses', function(event, data) {
+        self.setResponses(data);
+    });
+    $(document).bind('clientDisconnected', function(event, data) {
+        self.clientDisconnected(data);
+    });
+};
+
+RenderManager.prototype.setActive = function(mote) {
+    this.active_mote = mote;
+    this.display(this.active_mote);
+};
+
+RenderManager.prototype.newResponse = function(data) {
+    if(this.active_mote.pk == data.mote_id) {
+        var responses = {};
+        responses[data.client] = data.message;
+        if(this.renderer) {
+            this.renderer.updateData(responses, true);
+        }
+    }
+};
+
+RenderManager.prototype.setResponses = function(data) {
+    var responses = {};
+    $.each(data.response, function(client, response) {
+        try {
+            var response = JSON.parse(response);
+            responses[client] = response;
+        } catch(e) {}
+    });
+    if(this.renderer) {
+        this.renderer.updateData(responses, false);
+    }
+};
+
+RenderManager.prototype.display = function(mote) {
+    if(window[mote.content_type]) {
+        this.renderer = new window[mote.content_type](mote);
+        this.renderer.render();
+    }
+};
+
+RenderManager.prototype.clientDisconnected = function() {
+    if(this.renderer) {
+        this.renderer.clientDisconnected(data.client);
+    }
+};
